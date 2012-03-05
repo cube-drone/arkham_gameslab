a4f9bf201c98ebd682562a2a015dce46d926e026
Curtis Lassam <curtis@lassam.net>
2012-03-04 22:50:56 -0800

    Rewrote the deck generator to accept a list of arguments.

--- deck_gen/deck_generator.rb
+++ deck_gen/deck_generator.rb
@@ -9,15 +9,12 @@ options = {}
 optparse = OptionParser.new {|opts|
     opts.banner = "Usage: deck_generator.rb --file FILENAME"
 
-    options[:filename] = ""
-    opts.on( '-f', '--file FILENAME', 'File' ) {|file| options[:filename] = file } 
-
-    options[:name] = ""
-    opts.on( '-d', '--deck NAME', 'The name of the deck.') { |name| options[:name] = name } 
+    options[:filenames] = ""
+    opts.on( '-f', '--file a,b,c', Array, 'File' ) {|file| options[:filenames] = file } 
 
     options[:stylesheet] = "style.css" 
     opts.on( '-s', '--stylesheet FILENAME', 'The name of the stylesheet.') { |filename| options[:stylesheet] = filename } 
-
+    
     opts.on("-h", "--help", "Show this message"){
         puts opts
         exit
@@ -25,15 +22,10 @@ optparse = OptionParser.new {|opts|
 }
 optparse.parse!
 
-if (options[:filename] == "") then
+if (options[:filenames].length == 0) then
     puts optparse
     exit
 end
-
-# 2. Parse Deck
-thing = YAML.load_file( options[:filename] )
-
-pagecounter = 0
 
 # 3. Display Deck
 puts "<!DOCTYPE html>"
@@ -80,30 +72,39 @@ puts "\t<link rel='stylesheet' href='"+options[:stylesheet]+"' />"
 puts "<body>"
 puts "<div class='Page'>" 
 
-thing.each do |key, value|
-    # Quantity 
-    quantity = value["Quantity"] ||= 1
-    category = options[:name]
-    title = value["Title"] ||= key
-    image = value["Image"] ||= ""
-    special = value["Special"] ||= ""
-    flavor = value["Flavor"] ||= ""
-    type = value["Type"] ||= ""
-    remaining_values = value.reject{ |key, value| ["Quantity", "Title", "Image", "Special", "Flavor", "Type"].include?(key) }
-    sorted_remaining_values = remaining_values.sort{ |a, b| a[0].split(" ")[-1] <=> b[0].split(" ")[-1] } 
-    
-    quantity.times do 
-        puts "\t<div class='Card " + category + " " + title + " " + type + "'>"
-        puts "\t\t<h2 class='category'>" + category + " </h2>" unless category == ""
-        puts "\t\t<h3 class='title'>" + title + " </h3>"
-        puts "\t\t<img src='"+image+"' /> " unless image == "" 
-        puts "\t\t<p class='special'>" + special + "</p>" unless special == "" 
-        sorted_remaining_values.each { |other_value_name, other_value| puts "\t\t<p class='"+other_value_name.to_s.downcase + "'><strong>"+ other_value_name.to_s + "</strong>: " + other_value.to_s + " </p>" }
-        puts "\t\t<p class='flavor'><em>" + flavor + "</em></p>" unless flavor == "" 
-        puts "\t\t<div class='type'>" + type + "</div>" unless type == "" 
-        puts "\t</div>"
-        pagecounter += 1
-        puts "<div style='clear:both'> </div> </div><div class='Page'>" if pagecounter % CARDS_PER_PAGE == 0
+# 2. Parse Deck
+
+pagecounter = 0
+options[:filenames].each do |filename| 
+
+    thing = YAML.load_file( filename )
+
+    category = thing["Title"] ||= "Deck" 
+
+    thing.each do |key, value|
+        next if value.class == String
+        quantity = value["Quantity"] ||= 1
+        title = value["Title"] ||= key
+        image = value["Image"] ||= ""
+        special = value["Special"] ||= ""
+        flavor = value["Flavor"] ||= ""
+        type = value["Type"] ||= ""
+        remaining_values = value.reject{ |key, value| ["Quantity", "Title", "Image", "Special", "Flavor", "Type"].include?(key) }
+        sorted_remaining_values = remaining_values.sort{ |a, b| a[0].split(" ")[-1] <=> b[0].split(" ")[-1] } 
+        
+        quantity.times do 
+            puts "\t<div class='Card " + category + " " + title + " " + type + "'>"
+            puts "\t\t<h2 class='category'>" + category + " </h2>" unless category == ""
+            puts "\t\t<h3 class='title'>" + title + " </h3>"
+            puts "\t\t<img src='"+image+"' /> " unless image == "" 
+            puts "\t\t<p class='special'>" + special + "</p>" unless special == "" 
+            sorted_remaining_values.each { |other_value_name, other_value| puts "\t\t<p class='"+other_value_name.to_s.downcase + "'><strong>"+ other_value_name.to_s + "</strong>: " + other_value.to_s + " </p>" }
+            puts "\t\t<p class='flavor'><em>" + flavor + "</em></p>" unless flavor == "" 
+            puts "\t\t<div class='type'>" + type + "</div>" unless type == "" 
+            puts "\t</div>"
+            pagecounter += 1
+            puts "<div style='clear:both'> </div> </div><div class='Page'>" if pagecounter % CARDS_PER_PAGE == 0
+        end
     end
 end
 
