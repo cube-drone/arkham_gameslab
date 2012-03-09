d868f6b62c8e4429a375164d123d4c7d2fac9e8f
Curtis Lassam <curtis@lassam.net>
2012-03-08 21:35:26 -0800

    Drive-by styling

--- Phoenix/cards.bat
+++ Phoenix/cards.bat
@@ -2,4 +2,5 @@ rmdir /S /Q output
 mkdir output
 ruby ../deck_gen/deck_generator.rb --file combat_cards.yaml,strategic_objectives.yaml > output/everything.html
 copy deck_style.css output\style.css
+copy assets output\assets
 "C:\Program Files (x86)\wkhtmltopdf\wkhtmltopdf" output\everything.html output\master.pdf
