27396065d3d79b1c9e033f27b91d68ee1b728be7
Craig Fleming <rythos42@gmail.com>
2012-03-08 16:59:55 -0800

    Created cards YAML, clarified Attacked/Defender, wrote card generation script, created basic card fluff.

---
+++ Phoenix/cards.bat
@@ -0,0 +1,5 @@
+rmdir /S /Q output
+mkdir output
+ruby ../deck_gen/deck_generator.rb --file combat_cards.yaml,strategic_objectives.yaml > output/everything.html
+copy deck_style.css output\style.css
+"C:\Program Files (x86)\wkhtmltopdf\wkhtmltopdf" output\everything.html output\master.pdf
