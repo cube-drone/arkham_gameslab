7ab06d401d9bd26825b50181b4186d74e8726085
Craig Fleming <rythos42@gmail.com>
2012-03-09 17:00:35 -0800

    Fixed card scaling, added sector and production cards, added scaled backgrounds, list of changes that we talked about, changed "Combat" to "Leader"

--- Phoenix/cards.bat
+++ Phoenix/cards.bat
@@ -1,7 +1,7 @@
 rmdir /S /Q output
 mkdir output
 mkdir output\assets
-ruby ../deck_gen/deck_generator.rb -c 9 --file combat_cards.yaml,strategic_objectives.yaml > output/everything.html 
+ruby ../deck_gen/deck_generator.rb -c 8 --file combat_cards.yaml,strategic_objectives.yaml,sectors.yaml,production_turn.yaml > output/everything.html 
 copy deck_style.css output\style.css
 copy assets\*.* output\assets\*.*
-"C:\Program Files (x86)\wkhtmltopdf\wkhtmltopdf" output\everything.html output\master.pdf
+"C:\Program Files (x86)\wkhtmltopdf\wkhtmltopdf" -s Letter -O Landscape -B 0 -L 0 -R 0 -T 0 --disable-smart-shrinking output\everything.html output\master.pdf
