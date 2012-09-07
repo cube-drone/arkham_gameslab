9894338a7a9bc208af69a5caedb9f6737512a865
Craig Fleming <rythos42@gmail.com>
2012-09-07 10:45:35 -0700

    Draft 1 of cards and rules

--- Colony/cards.bat
+++ Colony/cards.bat
@@ -1,7 +1,7 @@
 rmdir /S /Q output
 mkdir output
 mkdir output\assets
-ruby ../deck_gen/deck_generator.rb -c 8 --file Colony.yaml > output/everything.html 
+ruby ../deck_gen/deck_generator.rb -c 8 --file Colony.yaml,Ship.yaml > output/everything.html 
 copy deck_style.css output\style.css
 copy assets\*.* output\assets\*.*
 "C:\Program Files (x86)\wkhtmltopdf\wkhtmltopdf" -s Letter -O Landscape -B 0 -L 0 -R 0 -T 0 --disable-smart-shrinking output\everything.html output\master.pdf
